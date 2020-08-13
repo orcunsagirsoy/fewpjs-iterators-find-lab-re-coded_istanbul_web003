9201b575c8cd9a456438dd718b71b744731b2f95
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-08-13 15:31:08 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array){
+
+  const winResult = array.find( result => result.result === 'W' );
+    console.log(winResult);
+
+    if(winResult !== undefined){
+      return winResult.year
+
+    }else{
+    return winResult
+  }
+}
